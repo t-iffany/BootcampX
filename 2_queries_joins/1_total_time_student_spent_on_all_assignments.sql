896c14248bff5014a5f014f5927fe79412a96869
t-iffany <tiffanyxleong@gmail.com>
2022-12-13 02:18:22 +0000

    Update query 1 and reload assignment_submissions_seeds.sql

--- 2_queries_joins/1_total_time_student_spent_on_all_assignments.sql
+++ 2_queries_joins/1_total_time_student_spent_on_all_assignments.sql
@@ -1,4 +1,4 @@
 SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
-JOIN students ON student_id = students.id
+JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
